1e56ac507e3f4f72b0d605a1a4f12782cd07c01d
avinashtandon <avinashtandon07@gmail.com>
2024-02-29 03:31:42 +0530

    working

--- Tic-Tac-Toe-Game/script.js
+++ Tic-Tac-Toe-Game/script.js
@@ -1,27 +1,92 @@
-let boxex = document.querySelectorAll(".box");
-let resetbtn = document.querySelector("#resetbtn");
-let turno = true;
-const winningpatter = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
+// Selecting HTML elements representing the tic-tac-toe game components
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-cont");
+let msg = document.querySelector("#msg");
 
-boxex.forEach((box) =>{ 
-    box.addEventListener("click",() =>{
-        if(turno === true){
-            box.innerHTML = "X";
-            turno = false;
-        }else{
-            box.innerHTML = "O";
-            turno = true;
-        }
-        box.disabled = true; // After a box is clicked, it is disabled. This line ensures that the box cannot be clicked again
-        checkWinner(); // Checking if someone has won the game after every move.
-    })
-})
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
+//Array defining winning patterns on the tic-tac-toe board
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+// Function to reset the game to its initial state
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes(); // Re-enable all the game boxes
+  msgContainer.classList.add("hide"); // Hide the message container
+};
+// Event listener for each box that gets triggered when a player makes a move
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
